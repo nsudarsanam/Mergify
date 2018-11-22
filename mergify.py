a34b9f42b9a4a124da5df5038c79e7f3a06affaf
nsudarsanam <nisha.sudarsanam@gmail.com>
2018-11-22 11:26:14 -0800

    Added caching of auth token

--- mergify.py
+++ mergify.py
@@ -1,6 +1,5 @@
 from flask import Flask, request,jsonify, make_response
 import requests
-app = Flask(__name__)
 import webbrowser
 from datetime import datetime, date, time
 import json
@@ -8,36 +7,36 @@ import string
 import io
 import csv
 import sys
+import atexit
 
 API_KEY = 'aa41ca35415dda68349438aabcb0da3d'
 SHARED_SECRET = '865ef2cb9f0b03b2627497b1c24b41a9'
+tokenFilename = 'tokens.json'
+tokens = {}
 
-class ShopifySession:
-    def __init__(self):
-        self.token = ""
-        self.store = ""
-    
-    def setToken(self,token):
-        self.token = token
+def writeAuthTokens():
+    with open(tokenFilename,'w') as tokenFile:
+        tokenFile.write(json.dumps(tokens))
 
-    def setStore(self,store):
-        self.store = store
+atexit.register(writeAuthTokens)
 
-    def getAdminStoreUrl(self):
-        return 'https://' + self.store + '/admin/'
+def startup():
+    global tokens
+    with open(tokenFilename) as file:
+        tokens = json.load(file)
 
-class ShopifyCustomer:
-    def __init__(self,customer):
-        self.__dict__ = customer
+def create_app():
+    startup()
+    return Flask(__name__)
 
-currentSession = ShopifySession()
+app = create_app()
 
 @app.route('/shopify')
 def shopify():
     print(request.args['shop'])
-    currentSession.setStore(request.args['shop'])
-    print(buildShopifyPermissionsStoreUrl(currentSession.store))
-    webbrowser.open(buildShopifyPermissionsStoreUrl(currentSession.store))
+    store = request.args['shop']
+    print(buildShopifyPermissionsStoreUrl(store))
+    webbrowser.open(buildShopifyPermissionsStoreUrl(store))
     return jsonify(success=True)
 
 @app.route('/redirect')
@@ -49,57 +48,51 @@ def redirect():
     print(hmac)
     print(store)
     print(request.args['timestamp'])
-    currentSession.setToken(getAuthToken(code,currentSession.store,hmac))
+    tokens[store] = getAuthToken(code,store,hmac)
     return jsonify(success=True)
 
 @app.route('/duplicates/customers')
 def findDuplicateCustomers():
-    customers = callShopify(currentSession.getAdminStoreUrl() + "customers.json", currentSession.token)['customers']
-    si = io.StringIO()
-    cw = csv.writer(si, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    cw.writerow(['Duplicate Customer', 'Original Customer', 'Duplicate Customer Link','Original Customer Link'])
-    foundDupes = False
-    for i in range(0,len(customers)):  # 0, 1, 2
-        firstCustomer = customers[i]
-        origCustomer = firstCustomer
-        for j in range(0,len(customers)): #1,2,3
-            if i == j:
-                continue
-            origCreatedAt = datetime.strptime(origCustomer['created_at'], "%Y-%m-%dT%H:%M:%SZ")  
-            secondCustomer = customers[j]
-            if areDuplicateCustomers(firstCustomer,secondCustomer):
-                secondCustomerCreatedAt = datetime.strptime(secondCustomer['created_at'], "%Y-%m-%dT%H:%M:%SZ")
-                if secondCustomerCreatedAt <= origCreatedAt:
-                    origCustomer = secondCustomer
-                    foundDupes = True
-        if origCustomer['id'] != firstCustomer['id']:
-            cw.writerow(generateDuplicateCustomer(firstCustomer,origCustomer))
+    store = request.args['shop']
+    if store in tokens:
+        customers = callShopify(getAdminStoreUrl(store) + "customers.json", tokens[store])['customers']
+        si = io.StringIO()
+        cw = csv.writer(si, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        cw.writerow(['Duplicate Customer', 'Original Customer', 'Duplicate Customer Link','Original Customer Link'])
+        foundDupes = False
+        for i in range(0,len(customers)):  
+            firstCustomer = customers[i]
+            origCustomer = firstCustomer
+            for j in range(0,len(customers)): 
+                if i == j:
+                    continue
+                origCreatedAt = datetime.strptime(origCustomer['created_at'], "%Y-%m-%dT%H:%M:%SZ")  
+                secondCustomer = customers[j]
+                if areDuplicateCustomers(firstCustomer,secondCustomer):
+                    secondCustomerCreatedAt = datetime.strptime(secondCustomer['created_at'], "%Y-%m-%dT%H:%M:%SZ")
+                    if secondCustomerCreatedAt <= origCreatedAt:
+                        origCustomer = secondCustomer
+                        foundDupes = True
+            if origCustomer['id'] != firstCustomer['id']:
+                cw.writerow(generateDuplicateCustomer(firstCustomer,origCustomer,store))
 
-    if foundDupes:
-        output = make_response(si.getvalue())
-        output.headers["Content-Disposition"] = "attachment; filename=duplicate_customers.csv"
-        output.headers["Content-type"] = "text/csv"
-        return output
-    else:
-        return jsonify(success=True)
+        if foundDupes:
+            output = make_response(si.getvalue())
+            output.headers["Content-Disposition"] = "attachment; filename=duplicate_customers.csv"
+            output.headers["Content-type"] = "text/csv"
+            return output
+        else:
+            return jsonify(success=True)
+    return jsonify(success=True)
 
-def generateDuplicateCustomer(currentCustomer,origCustomer):
-    # t1 = datetime.strptime(firstCustomer['created_at'], "%Y-%m-%dT%H:%M:%SZ")
-    # t2 = datetime.strptime(secondCustomer['created_at'], "%Y-%m-%dT%H:%M:%SZ")
+def generateDuplicateCustomer(currentCustomer,origCustomer,store):
     currentCustomerName = currentCustomer['first_name'] + ' ' + currentCustomer['last_name']
     origCustomerName = origCustomer['first_name'] + ' ' + origCustomer['last_name']
-    currentCustomerUrl = currentSession.getAdminStoreUrl() + 'customers/' + str(currentCustomer['id'])
-    origCustomerUrl = currentSession.getAdminStoreUrl() + 'customers/' + str(origCustomer['id'])
+    currentCustomerUrl = getAdminStoreUrl(store) + 'customers/' + str(currentCustomer['id'])
+    origCustomerUrl = getAdminStoreUrl(store) + 'customers/' + str(origCustomer['id'])
 
     return [currentCustomerName,origCustomerName,currentCustomerUrl,origCustomerUrl]
-    # if t1 <= t2:
-    #     #customer2 is duplicate of customer1
-    #     return [secondCustomerName,firstCustomerName,secondCustomerUrl,firstCustomerUrl]
-    # else:
-    #     #customer1 is duplicate of customer2
-    #     #Duplicate customer name, Original Customer name, Duplicate customer url, Original customer url
-    #     return [firstCustomerName,secondCustomerName,firstCustomerUrl,secondCustomerUrl]
-
+    
 #[{"DuplicateCustomerName","OriginalCustomerName","DuplicateCustomerUrl","OriginalCustomerUrl"}]
 def areDuplicateCustomers(firstCustomer, secondCustomer):
     # if 2 people have the same phone number OR same address - 1) tag as duplicate 2) add note 3) apply metadata field
@@ -139,39 +132,46 @@ def callShopify(url,authToken):
 
 
 def getAuthToken(code,storename,hmac):
-    url = currentSession.getAdminStoreUrl() + 'oauth/access_token'
+    url = getAdminStoreUrl(storename) + 'oauth/access_token'
     data = {'client_id':API_KEY,'client_secret':SHARED_SECRET,'code':code, 'hmac':hmac}
     print(data)
     r = requests.post(url, data=data)
     print(r.text)
     print(r.json()['access_token'])
     return r.json()['access_token']
-  
+
 def buildShopifyPermissionsStoreUrl(storename):
     scope="read_customers,write_customers,read_orders,write_orders"
-    return currentSession.getAdminStoreUrl() + '/oauth/authorize?client_id=' + API_KEY + '&scope='+ scope +'&redirect_uri=' + getRedirectUri() 
-
-# def getAdminStoreUrl(storename):
-#     return 'https://' + storename + '/admin/'
+    return getAdminStoreUrl(storename) + '/oauth/authorize?client_id=' + API_KEY + '&scope='+ scope +'&redirect_uri=' + getRedirectUri() 
 
 def getRedirectUri():
-    return 'https://8026c3ee.ngrok.io/redirect'
+    return 'https://c5a11afa.ngrok.io/redirect'
+
+def getAdminStoreUrl(store):
+        return 'https://' + store + '/admin/'
+
    
 
 
 # /*
 # TODO:
-## on callbacks
-# # verify nonce
-## verify hmac
-## validate store name
 ## load test apis
 ## pagination of apis
+    ## bulk create and delete customers
+## add support for orders
+## add buttons for export
+
+#V1.1:
+## on callbacks
+    # # verify nonce
+    ## verify hmac
+    ## validate store name
 
 #V2
  # tag the later customer as dupe of oldest
-    # add note
-    # add metadata
+# add note
+# add metadata
+
    
 # */
 
